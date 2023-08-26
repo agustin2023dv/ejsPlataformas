2f17df1df9e0a97e8839fffc9164aa24e7b7d706
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-08-26 02:33:27 +0200

    Ejercicios vistos en clases

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Primer ejercicio
+/*
+Console.WriteLine("Ingrese su nombre:");
+String nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su telefono:");
+String telefono = Console.ReadLine();
+Console.WriteLine("Ingrese su edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese su fecha de nacimiento:");
+DateTime fechaNacimiento = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+
+Console.WriteLine("Bienvenido {0}!\nTelefono: {1}\nEdad: {2}\nFecha de nacimiento: {3:dd/MM/yyyy}", nombre, telefono, edad, fechaNacimiento);*/
+
+//Segundo ejercicio. Tickets
+
+/*Console.WriteLine("Ingrese el numero de ticket:");
+int nroTicket = Convert.ToInt32(Console.ReadLine);
+
+Console.WriteLine("Ingrese nombre del cliente:");
+String nombreCliente = Console.ReadLine();
+
+Console.WriteLine("Ingrese la fecha:");
+DateTime fecha = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+Console.WriteLine("Ingrese el codigo:");
+int codigo = Convert.ToInt32(Console.ReadLine);
+
+Console.WriteLine("Ingrese la descripcion:");
+String descripcion = Console.ReadLine();
+
+Console.WriteLine("Ingrese la marca:");
+String marca = Console.ReadLine();
+
+Console.WriteLine("Ingrese el precio:");
+double precio = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Numero de ticket: {0}!\nCliente: {1}\nFecha:{2:dd/MM/yyyy}\nCodigo: {3}\nDescripcion: {4}\nMarca:{5}\nPrecio: {6}", nroTicket, 
+   nombreCliente, fecha,codigo, marca,precio);*/
+
